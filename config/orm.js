238d0aede4799454e9a2dfac16fc824b528b9558
Faduma Abdillahi <faduma92@gmail.com>
2020-08-26 21:20:49 -0400

    set up connection and orm functions and schema

---
+++ config/orm.js
@@ -0,0 +1,63 @@
+const connection = require('./connection');
+
+const orm = {
+    selectAll: function (cb) {
+        let querySearch = 'SELECT * FROM burgers';
+
+        connection.query(
+            querySearch,
+
+            function (err, data) {
+                if (err) throw err;
+
+                cb(data);
+            }
+        );
+    },
+
+    insertOne: function(col, val, cb) {
+        let querySearch = `INSERT INTO burgers (${col}) VALUES (?)`;
+
+        connection.query(
+            querySearch,
+
+            val,
+            
+            function(err, data) {
+                if (err) throw err;
+
+                cb(data);
+            }
+        );
+    },
+
+    updateOne: function(val, id, cb) {
+        let querySearch = `UPDATE burgers SET devoured=${val} WHERE id = ${id}`;
+
+        connection.query(
+            querySearch,
+
+            function(err, data) {
+                if (err) throw err;
+    
+                cb(data);
+            }
+        );
+    },
+
+    deleteOne: function(id, cb) {
+        let querySearch = `DELETE FROM burgers WHERE id="${id}"`;
+
+        connection.query(
+            querySearch,
+
+            function(err, data) {
+                if (err) throw err;
+
+                cb(data);
+            }
+        );
+    }
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Junior chicken burger', false), ('Double stack cheeseburger', false), ('Beyond Meat burger', true);
